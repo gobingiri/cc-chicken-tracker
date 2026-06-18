4968f7722473fa3f86c1c56d9a681ac65a1fcb40
Gibson Gaines <gibsongaines@Gibsons-MacBook-Pro.local>
2026-06-18 12:23:51 -0400

    clean up test file

--- test_fetch.js
+++
@@ -1,6 +0,0 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxc68wix3qo-dMiUOPaHSVaSVKF-zu4XjFj8NXVlpBsxam4co0v4ZayA84W60kcLEw5sQ/exec';
-
-fetch(SCRIPT_URL)
-  .then(res => res.json())
-  .then(data => console.log(JSON.stringify(data, null, 2)))
-  .catch(err => console.error(err));
